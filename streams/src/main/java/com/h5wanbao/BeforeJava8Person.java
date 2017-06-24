37f600544de7bde4999741068bea61c32b332eff
Scott <1028282161@qq.com>
2017-06-24 15:57:55 +0800

    2.1 Before Java 8, you get a Person by name like this :

---
+++ streams/src/main/java/com/h5wanbao/BeforeJava8Person.java
@@ -0,0 +1,61 @@
+package com.h5wanbao;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author scott
+ * @Version 0.0.1
+ * Date: 2017/6/24 15:39
+ * ProjectName: java8
+ */
+public class BeforeJava8Person {
+
+    /*public static void main(String[] args) {
+
+        List<Person> persons = Arrays.asList(
+            new Person("Scott", 30),
+            new Person("Jhoon", 20),
+            new Person("Frank", 40)
+        );
+
+        Person result = getStudentByName(persons,"Jhoon");
+        System.out.println(result);
+
+    }
+
+    private static Person getStudentByName(List<Person> persons, String name) {
+        Person result = null;
+
+        for (Person temp : persons){
+            if(name.equals(temp.getName())){
+                result = temp;
+            }
+        }
+        return result;
+    }*/
+    public static void main(String[] args) {
+
+        List<Person> persons = Arrays.asList(
+                new Person("mkyong", 30),
+                new Person("jack", 20),
+                new Person("lawrence", 40)
+        );
+
+        Person result = getStudentByName(persons, "jack");
+        System.out.println(result);
+
+    }
+
+    private static Person getStudentByName(List<Person> persons, String name) {
+
+        Person result = null;
+        for (Person temp : persons) {
+            if (name.equals(temp.getName())) {
+                result = temp;
+            }
+        }
+        return result;
+    }
+
+}
